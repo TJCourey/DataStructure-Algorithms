0d02f143aa72300d0d6224684eda21da2db3cd40
TJ Courey <tjcourey84@gmail.com>
2022-06-10 20:21:15 -0600

    cleaned main repository

--- main.js
+++ main.js
@@ -1,23 +1,2 @@
-// takes currency minus $
-// return array with dollars and cents as separate positions in array
-// if currency improperly formatted (only num or .) return [0, 0]
-function currencySplit(cur) {
-  const totalArr = [];
-  let splitCur = cur.split(".");
-  let dollars = splitCur[0];
-  dollars = Number(
-    dollars
-      .split("")
-      .filter((char) => char !== ",")
-      .join("")
-  );
-  const cents = Number(splitCur[1] || 0);
-  if (isNaN(dollars) || isNaN(cents)) {
-    totalArr.push(0, 0);
-  } else {
-    totalArr.push(dollars, cents);
-  }
-  return totalArr;
-}
 
 module.exports = currencySplit;
