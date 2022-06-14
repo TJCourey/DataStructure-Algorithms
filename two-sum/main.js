1346c24597f058e49d3c0ea1dce42a083bfb618f
TJ Courey <tjcourey84@gmail.com>
2022-06-13 19:14:14 -0600

    started working two sum again

--- two-sum/main.js
+++ two-sum/main.js
@@ -1,3 +1,16 @@
-function main(param) {}
+//Create a function that takes in an array and an interger
+//cyle through array
+// find two number in array that equal integer
+// return location of two numbers in array
+function twoSum(arr, sum) {
+    let otptionArr = []
+    if (sum = isNaN) {
+        return ("Please enter a numerical Value" )
+    }
+    otptionArr.forEach(element => {
+        
+        
+    });
+}
 
-module.exports = main;
+module.exports = twoSum;
